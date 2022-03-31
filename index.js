21757db0742022fe7e7cdf29d8eb49b56dad6fec
NahataiSirisak <u6214600@au.edu>
2022-04-01 00:14:05 +0700

    t2a

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
@@ -37,5 +38,22 @@ console.debug(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log(`Server running on port: ${port}`));
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 module.exports = app;
